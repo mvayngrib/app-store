54776282475407491004bbac01ab2e1a332da552
Mark Vayngrib <mark@tradle.io>
2016-09-02 19:17:51 -0400

    mv files around

--- render.js
+++ render.js
@@ -1,7 +1,7 @@
 
 const fs = require('fs')
 const Handlebars = require('handlebars')
-const template = Handlebars.compile(fs.readFileSync('./index.hbs', { encoding: 'utf8' }))
+const template = Handlebars.compile(fs.readFileSync('./templates/index.hbs', { encoding: 'utf8' }))
 const html = template(require('./data'))
 
 fs.writeFileSync('./index.html', html)
