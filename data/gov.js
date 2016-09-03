aeca58500aff2f9a574addb7c23d083929de120a
pgmemk <ellen.katsnelson@lablz.com>
2016-09-02 21:56:00 -0400

    real QR

--- data/gov.js
+++ data/gov.js
@@ -8,16 +8,16 @@ module.exports = [
   {
     img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/jeremiah-wilson-2.jpg',
     caption: 'Company and activity license',
-    qr: '5;http://192.168.0.20;caal'
+    qr: '5;http://192.168.0.20:44444;caal'
   },
   {
     img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/jeremiah-wilson-3.jpg',
     caption: 'Culture and leisure',
-    qr: '5;http://192.168.0.20;cal'
+    qr: '5;http://192.168.0.20:44444;cal'
   },
   {
     img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/jeremiah-wilson-4.jpg',
     caption: 'Environment',
-    qr: '5;http://192.168.0.20;environment'
+    qr: '5;http://192.168.0.20:44444;environment'
   }
 ]
--- data/other.js
+++ data/other.js
@@ -3,11 +3,11 @@ module.exports = [
   {
     img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/jeremiah-wilson-1.jpg',
     caption: 'Crowdfunder',
-    qr: '5;http://192.168.0.20;crowdfunder'
+    qr: '5;http://192.168.0.20:44444;crowdfunder'
   },
   {
     img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/jeremiah-wilson-2.jpg',
     caption: 'LHV Bank',
-    qr: '5;http://192.168.0.20;lhv'
+    qr: '5;http://192.168.0.20:44444;lhv'
   },
 ]
