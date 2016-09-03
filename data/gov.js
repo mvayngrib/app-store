3f36d1a4e1132065709bcc42523540301f9a9606
pgmemk <ellen.katsnelson@lablz.com>
2016-09-02 21:38:23 -0400

    real QR

--- data/gov.js
+++ data/gov.js
@@ -3,21 +3,21 @@ module.exports = [
   {
     img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/jeremiah-wilson-1.jpg',
     caption: 'E-residency',
-    qr: '5;http://192.168.0.20/eresidency'
+    qr: '5;http://192.168.0.20;eresidency'
   },
   {
     img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/jeremiah-wilson-2.jpg',
     caption: 'Company and activity license',
-    qr: '5;http://192.168.0.20/eresidency'
+    qr: '5;http://192.168.0.20;caal'
   },
   {
     img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/jeremiah-wilson-3.jpg',
     caption: 'Culture and leisure',
-    qr: '5;http://192.168.0.20/cultureAndLeisure'
+    qr: '5;http://192.168.0.20;cal'
   },
   {
     img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/jeremiah-wilson-4.jpg',
     caption: 'Environment',
-    qr: '5;http://192.168.0.20/environment'
+    qr: '5;http://192.168.0.20;environment'
   }
 ]
