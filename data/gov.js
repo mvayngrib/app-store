cbf3c451efecefb845a2324e44ec2d41b906e48c
Mark Vayngrib <mark@tradle.io>
2016-09-02 22:41:03 -0400

    images

--- data/gov.js
+++ data/gov.js
@@ -1,22 +1,22 @@
 
 module.exports = [
   {
-    img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/jeremiah-wilson-1.jpg',
+    img: 'images/eresidency.png',
     caption: 'E-residency',
     qr: '5;http://192.168.0.20;eresidency'
   },
   {
-    img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/jeremiah-wilson-2.jpg',
-    caption: 'Company and activity license',
+    img: 'images/caal1.png',
+    caption: 'Company and activity licence',
     qr: '5;http://192.168.0.20:44444;caal'
   },
   {
-    img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/jeremiah-wilson-3.jpg',
+    img: 'images/cal1.png',
     caption: 'Culture and leisure',
     qr: '5;http://192.168.0.20:44444;cal'
   },
   {
-    img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/jeremiah-wilson-4.jpg',
+    img: 'images/environment1.png',
     caption: 'Environment',
     qr: '5;http://192.168.0.20:44444;environment'
   }
--- data/other.js
+++ data/other.js
@@ -1,13 +1,13 @@
 
 module.exports = [
   {
-    img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/jeremiah-wilson-1.jpg',
-    caption: 'Crowdfunder',
-    qr: '5;http://192.168.0.20:44444;crowdfunder'
-  },
-  {
     img: 'images/lhv.jpg',
     caption: 'LHV Bank',
     qr: '5;http://192.168.0.20:44444;lhv'
+  },
+  {
+    img: 'images/crowdfunder.png',
+    caption: 'Crowdfunder',
+    qr: '5;http://192.168.0.20:44444;crowdfunder'
   },
 ]
