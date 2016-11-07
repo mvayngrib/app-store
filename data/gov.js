d04e9022a0145393c83e54f4f0769fc0564c6fef
Mark Vayngrib <mark@tradle.io>
2016-11-07 17:41:13 -0500

    change caption to title/desciption

--- data/gov.js
+++ data/gov.js
@@ -4,12 +4,14 @@ const base = process.argv[2] || 'http://localhost'
 module.exports = [
   {
     img: 'images/singpass.jpg',
-    caption: 'E-Identity',
+    title: 'e-Identity',
+    description: 'establish your Singaporean digital identity',
     qr: `5;${base};seres`
   },
   {
     img: 'images/rocket.jpg',
-    caption: 'Register a Company',
+    title: 'BizReg',
+    description: 'register your company',
     qr: `5;${base};sregco`
   }
 ]
--- data/other.js
+++ data/other.js
@@ -4,31 +4,38 @@ const base = process.argv[2] || 'http://localhost'
 module.exports = [
   {
     img: 'images/notary.jpg',
-    caption: 'Notary',
-    by: 'Singapore Notaries',
+    title: 'Notariiize',
+    description: 'Notarize any kind of documents',
     qr: `5;${base};snotary`
   },
   {
     img: 'images/contract1.jpg',
-    caption: 'Contract Machine',
-    by: 'Iron Clad',
+    title: 'Shake',
+    description: 'Create and sign contracts',
     qr: `5;${base};scontracts`
+  },
+  {
+    img: 'images/corpAction.png',
+    title: 'CorpTask',
+    description: 'Perform corporate actions from M&A to e-shares',
+    qr: `5;${base};scorpact`
+  },
+  {
+    img: 'images/loans.jpg',
+    title: 'Borrow More',
+    description: 'Low interest loans inside',
+    qr: `5;${base};sloans`
   },
   // {
   //   img: 'images/guarantee.jpg',
-  //   caption: 'Bank guarantee',
-  //   qr: `5;${base};guarantee`
+  //   title: '$ Guarantee',
+  //   description: 'Friendly Bank',
+  //   qr: `5;${base};sloans`
   // },
   {
-    img: 'images/loans.jpg',
-    caption: 'Borrow More',
-    by: 'Friendly Bank',
-    qr: `5;${base};sloans`
-  },
-  {
     img: 'images/charity.jpg',
-    caption: 'Sponsor a Child',
-    by: 'Charitee',
+    title: 'The Giver',
+    description: 'Sponsor a child, save a life',
     qr: `5;${base};scharity`
   }
 ]
